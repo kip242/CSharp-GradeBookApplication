1cee9a8101035a6c967851213f580e60a5c774c1
Kip Tatum <kip.tatum@cherwell.com>
2019-05-21 07:57:27 -0700

    Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -28,7 +28,30 @@ namespace GradeBook.GradeBooks
 				return 'D';
 			else
 				return 'F';
-				
+		}
+
+		public override void CalculateStatistics()
+		{
+			if (Students.Count < 5)
+			{
+				Console.WriteLine("Ranked Grading requires at least 6 students with grades in order to properly calculate a student's overall grade.");
+				return;
+			}
+			else
+			{
+				base.CalculateStatistics();
+			}
+		}
+
+		public override void CalculateStudentStatistics(string name)
+		{
+			if (Students.Count < 5)
+			{
+				Console.WriteLine("Ranked grading requires at least 5 students with grades in order to prperly calculate a student's overall grade.");
+				return;
+			}
+			else
+			base.CalculateStudentStatistics(name);
 		}
 	}
 }
